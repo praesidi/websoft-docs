3286991334a97f4607402ff2f26c2ba73e5e692a
Elmir Khalilov <edik.halilov1999@gmail.com>
2024-06-26 11:45:32 +0300

    feat: set up project and add pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>WebSoft Modern Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/praesidi/websoft-docs',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  docsRepositoryBase: 'https://github.com/praesidi/websoft-docs',
 }
 
 export default config
